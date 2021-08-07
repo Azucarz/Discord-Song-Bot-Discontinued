9bf2400aec78c93a04d6f66dc66eaf996ff5f43d
Dharshan <70443897+Dharshan@users.noreply.github.com>
2021-08-07 22:43:31 +0800

    Update Main.js
    update port

--- Main.js
+++ Main.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 
 app.listen(() => console.log('phantom Bot Ready !'));
+app.listen(process.env.PORT || 8080);
 
 app.use('/ping', (req, res) => {
   res.send(new Date());
