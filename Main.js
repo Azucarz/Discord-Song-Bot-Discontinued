42d398cbfc5e068df9486f74198b0d4fbd806274
Dharshan <70443897+Dharshan@users.noreply.github.com>
2021-08-08 13:45:41 +0800

    bot changes

--- Main.js
+++ Main.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.listen(() => console.log('phantom Bot Ready !'));
+app.listen(() => console.log('SBot Bot Ready !'));
 app.listen(process.env.PORT || 8080);
 
 app.use('/ping', (req, res) => {
